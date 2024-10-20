f8176a291c40a39c20244fc78e5680cdf191a442
Deepak <deepak@Deepaks-MacBook-Air.local>
2024-10-20 06:18:24 +0530

    testing

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,14 @@ import { errorHandler } from "../utils/errorHandler.js";
 import jwt from "jsonwebtoken";
 export const isAuthenticated = async (req, res, next) => {
   // implement cookie later
-  const token = req.headers.authorization.split(" ")[1];
-  if (!token) {
-    return errorHandler("token not found", 401, req, res);
-  }
-  const decodedData = await jwt.verify(token, process.env.JWT_SECRET);
-  if (!decodedData) {
-    return errorHandler("invalid access token", 401, req, res);
-  }
-  req.userId = decodedData._id;
+  // const token = req.headers.authorization.split(" ")[1];
+  // if (!token) {
+  //   return errorHandler("token not found", 401, req, res);
+  // }
+  // const decodedData = await jwt.verify(token, process.env.JWT_SECRET);
+  // if (!decodedData) {
+  //   return errorHandler("invalid access token", 401, req, res);
+  // }
+  // req.userId = decodedData._id;
   next();
 };
